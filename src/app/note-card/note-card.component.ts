1da616415273ddf975e767b9f29c82991726c799
Marina Hernando Izurza <Marina Hernando>
2023-07-16 12:09:01 +0200

    UI

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss'],
+})
+export class NoteCardComponent implements OnInit {
+
+  @Input() title!: string;
+  @Input() body!: string;
+
+  @ViewChild('truncator', { static: true }) truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText', { static: true }) bodyText!: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    // work out if there ir a text over and if not, then hide the truncator
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewwaleHeight = parseInt(style.getPropertyValue('height'), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewwaleHeight) {
+      // if there is a text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      // else (there ir a text overflow), hide the fade ut truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+}
