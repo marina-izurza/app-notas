ec75312eee8d33d3f2d34e870bfc6df2c2fb4979
Marina Hernando Izurza <Marina Hernando>
2023-07-16 14:07:35 +0200

    Angular animations

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,11 @@
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+  query,
+  stagger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -5,13 +13,95 @@ import { NotesService } from 'src/app/shared/notes.service';
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      // entry animation
+      transition('void => *', [
+        // initial state
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          // We hace to 'expand' out the padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingRight: 0,
+          paddingLeft: 0,
+        }),
+        // First we want to animate the spacinf (wich includes height and margin)
+        animate(
+          '50ms',
+          style({
+            height: '*',
+            'margin-bottom': '*',
+            paddingTop: '*',
+            paddingBottom: '*',
+            paddingRight: '*',
+            paddingLeft: '*',
+          })
+        ),
+        animate(100),
+      ]),
+      transition('* => void', [
+        // first scale up
+        animate(
+          50,
+          style({
+            transform: 'scale(1.05)',
+          })
+        ),
+        // the escale down back to normal size while beginning to fade out
+        animate(
+          50,
+          style({
+            transform: 'scale(1)',
+            opacity: 0.75,
+          })
+        ),
+        // Scale down and fade out completely
+        animate(
+          '120ms ease-out',
+          style({
+            opacity: 0,
+            transform: 'scale(0.68)',
+          })
+        ),
+        // Them animate the spacing (wich includes height, margin and padding)
+        animate(
+          '150ms ease-out',
+          style({
+            height: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingRight: 0,
+            paddingLeft: 0,
+            'margin-bottom': '0',
+          })
+        ),
+      ]),
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(':enter', [
+          style({
+            opacity: 0,
+            height: 0,
+          }),
+          stagger(100, [animate('0.2s ease')]),
+        ], {
+          optional: true
+        }),
+      ]),
+    ]),
+  ],
 })
 export class NotesListComponent implements OnInit {
-
   notes: Note[] = new Array<Note>();
 
-  constructor (private notesService: NotesService) { }
+  constructor(private notesService: NotesService) {}
 
   ngOnInit(): void {
     // We want to retreve all notes from NoteService
